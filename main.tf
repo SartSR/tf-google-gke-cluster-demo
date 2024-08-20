57f58121ad12293d88d9a0ad05fa2dc46801965a
SartSR <sart.sr@gmail.com>
2024-08-20 19:45:18 +0000

    bucket add

--- main.tf
+++ main.tf
@@ -2,28 +2,12 @@ module "gke_cluster" {
   source         = "github.com/SartSR/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
+  GKE_NUM_NODES  = 1
+  GKE_MACHINE_TYPE = var.GKE_MACHINE_TYPE
 }
-
-resource "google_container_cluster" "main" {
-  name     = var.GKE_CLUSTER_NAME
-  location = var.GOOGLE_REGION
-
-  initial_node_count       = 1
-  remove_default_node_pool = true
-}
-
-resource "google_container_node_pool" "main" {
-  name       = var.GKE_POOL_NAME
-  project    = google_container_cluster.main.project
-  cluster    = google_container_cluster.main.name
-  location   = google_container_cluster.main.location
-  node_count = var.GKE_NUM_NODES
-
-  node_config {
-    machine_type = var.GKE_MACHINE_TYPE
+terraform {
+  backend "gcs" {
+    bucket = "tfstatebckt"
+    prefix = "terraform/state"
   }
 }
-
-
-
